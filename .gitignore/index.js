66b2354bcf0788e00ae4ab2e9204cae89184e456
ShadowShoot <47925450+ShadowShoot@users.noreply.github.com>
2019-02-23 18:45:19 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    bot.user.setGame("Administrator, !help");
+    console.log("Le bot a bien été connecte");
+});
+
+bot.login("NTQ2MjgzNTE5MDgxNzA5NTY5.D1MTJg.qxvis5SQ-AgqC6InlSLtXnW-B0U");
